d51a21fe1197388bf9909c4331e8a504139a765a
Suphal Bochkar <suphalbochkar@gmail.com>
2023-08-02 22:05:35 +0530

    Arr

---
+++ DSA/Unit - 1/L2_5.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int un_arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        int num = arr[i];
+        for (int j = i + 1; j < n; j++)
+        {
+            
+        }
+    }
+}
