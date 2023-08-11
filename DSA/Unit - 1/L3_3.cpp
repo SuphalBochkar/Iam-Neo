923b34bc37cea588ebeb9eb00d04f6916bc372e3
Suphal Bochkar <suphalbochkar@gmail.com>
2023-08-11 14:32:29 +0530

    Mid Insert

--- DSA/Unit - 1/L3_3.cpp
+++ DSA/Unit - 1/L3_3.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void middle_insert(vector<int> &arr, int &n, int q, int k)
+{
+    if (q >= 1 && q <= n + 1)
+    {
+        for (int i = n; i > q - 1; i--)
+        {
+            arr[i] = arr[i - 1];
+        }
+        arr[q - 1] = k;
+        n++;
+    }
+}
+
 int main()
 {
     int n;
@@ -22,6 +35,22 @@ int main()
     }
 
     cout << endl;
+
+    if (index >= 1 && index <= n + 1)
+    {
+        middle_insert(arr, n, index, key);
+
+        cout << "Updated array: ";
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+    else
+    {
+        cout << "Invalid position!";
+    }
+
     return 0;
 }
 
