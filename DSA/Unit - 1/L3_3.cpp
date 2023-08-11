a9c5134d19f20e5f39cdb87730bfe7a726603942
Suphal Bochkar <suphalbochkar@gmail.com>
2023-08-11 14:32:47 +0530

    In

--- DSA/Unit - 1/L3_3.cpp
+++ DSA/Unit - 1/L3_3.cpp
@@ -54,3 +54,27 @@ int main()
     return 0;
 }
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int index, key;
+    cin >> index >> key;
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
