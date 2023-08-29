31b029c887695478dabf2d69e93b54a9dbed704c
Suphal Bochkar <suphalbochkar@gmail.com>
2023-08-29 13:40:36 +0530

    1st - Occ

--- DSA/Unit - 1/L5_4.cpp
+++ DSA/Unit - 1/L5_4.cpp
@@ -1,6 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int firstOcc(vector<int> &arr, int n, int k)
+{
+    int s = 0, e = n - 1, leftOcc = -1;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (arr[mid] == k)
+        {
+            leftOcc = mid;
+            e = mid - 1;
+        }
+        else if (k < arr[mid])
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return leftOcc;
+}
 int main()
 {
     int n;
