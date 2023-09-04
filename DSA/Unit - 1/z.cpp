24fe762b585231875246ff06b3b0a5abf0cf93d3
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-04 18:29:03 +0530

    Temp Z

--- DSA/Unit - 1/z.cpp
+++ DSA/Unit - 1/z.cpp
@@ -1,6 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class NumberReverse
+{
+public:
+    int reverse(int n)
+    {
+        int reversed = 0;
+        while (n > 0)
+        {
+            reversed = reversed * 10 + n % 10;
+            n /= 10;
+        }
+        return reversed;
+    }
+};
+
+int reverse(int n)
+{
+    int reversed = 0;
+    while (n > 0)
+    {
+        reversed = reversed * 10 + n % 10;
+        n /= 10;
+    }
+    return reversed;
+}
 
 int main()
 {
@@ -17,4 +42,29 @@ int main()
     int input3 = 1050;
     int output3 = nr.reverse(input3);
     std::cout << "Input 3: " << input3 << "\nOutput 3: " << output3 << std::endl;
+
+    return 0;
+
+    vector<int> arr;
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+
+    for (int i : arr)
+    {
+        cout << i;
+    }
 }
