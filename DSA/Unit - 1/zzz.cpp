5872b31901aeb40d8b8750ae24d0b20f9903de37
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-04 18:30:25 +0530

    Class Set Song

--- DSA/Unit - 1/zzz.cpp
+++ DSA/Unit - 1/zzz.cpp
@@ -2,6 +2,72 @@
 #include <iomanip>
 using namespace std;
 
+class Song
+{
+private:
+    string artist;
+    string title;
+    string album;
+    int play_count = 0;
+    const double pay_rate = 0.001;
+    double money_earned = 0;
+
+public:
+    // Constructor
+    Song(string artist, string title, string album)
+    {
+        this->artist = artist;
+        this->title = title;
+        this->album = album;
+    }
+
+    // Getter functions
+    string GetArtist()
+    {
+        return artist;
+    }
+
+    string GetTitle()
+    {
+        return title;
+    }
+
+    string GetAlbum()
+    {
+        return album;
+    }
+
+    int GetPlayCount()
+    {
+        return play_count;
+    }
+
+    double GetPayRate()
+    {
+        return pay_rate;
+    }
+
+    double GetMoneyEarned()
+    {
+        return money_earned;
+    }
+
+    // Setter functions
+    void SetArtist(string artist)
+    {
+        this->artist = artist;
+    }
+
+    void SetTitle(string title)
+    {
+        this->title = title;
+    }
+
+    void SetAlbum(string album)
+    {
+        this->album = album;
+    }
+
 int main()
 {
 
