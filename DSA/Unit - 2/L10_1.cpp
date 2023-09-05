c9410ffab0bfae00998bddb3ccf06e999a876e35
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-05 23:32:43 +0530

    LL - Fun

--- DSA/Unit - 2/L10_1.cpp
+++ DSA/Unit - 2/L10_1.cpp
@@ -36,3 +36,56 @@ int main()
     cout << "\nModified Linked List: ";
     display(list);
 }
+
+void addNode(node *&list, int val)
+{
+    node *n = new node(val);
+    if (list == nullptr)
+    {
+        list = n;
+        return;
+    }
+    else
+    {
+        node *ptr = list;
+        while (ptr->next != nullptr)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = n;
+    }
+}
+
+void removeNodes(node *&list, int x)
+{
+    node dummy(0);
+    dummy.next = list;
+    node *prev = &dummy;
+    node *current = list;
+    while (current)
+    {
+        if (current->data > x)
+        {
+            prev->next = current->next;
+            current = prev->next;
+        }
+        else
+        {
+            prev = current;
+            current = current->next;
+        }
+    }
+    list = dummy.next;
+}
+
+void display(node *n)
+{
+    for (node *ptr = n; ptr != nullptr; ptr = ptr->next)
+    {
+        cout << ptr->data;
+        if (ptr->next != nullptr)
+        {
+            cout << " ";
+        }
+    }
+}
