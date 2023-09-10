56d0d0bb09eac49b690b4836c791fdfaa09513ba
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-10 16:53:54 +0530

    Matrices - Out

--- OOPS/Unit - 2/L10_3.cpp
+++ OOPS/Unit - 2/L10_3.cpp
@@ -1,6 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Product
+{
+private:
+    string name;
+    float price;
+    int qua;
+
+public:
+    void getData(string a, float b, int c)
+    {
+        name = a;
+        price = b;
+        qua = c;
+    }
+
+    float getPrice()
+    {
+        return price * qua;
+    }
+
+    void details()
+    {
+        cout << "Product Name: " << name << endl;
+        cout << "Total Price: " << fixed << setprecision(2) << getPrice() << endl;
+    }
+};
+
 int main()
 {
 
@@ -25,6 +52,16 @@ int main()
         pro->getData(a, b, c);
         cart.push_back(pro);
     }
+
+    cout << "Shopping Cart details:" << endl;
+
+    float tot = 0.0;
+    for (int i = 0; i < n; i++)
+    {
+        cart[i]->details();
+        tot += cart[i]->getPrice();
+    }
+
     return 0;
 }
 
