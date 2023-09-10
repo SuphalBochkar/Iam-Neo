a4666927618c3d88a67203eb35ed7bea2afde54a
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-10 16:52:11 +0530

    Matrices - Out

--- OOPS/Unit - 2/L11_1.cpp
+++ OOPS/Unit - 2/L11_1.cpp
@@ -23,4 +23,28 @@ int main()
             cin >> b[i][j];
         }
     }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int row = 0;
+            for (int k = 0; k < n; k++)
+            {
+                row += a[i][k] * b[k][j];
+            }
+            pro[i][j] = row;
+        }
+    }
+
+    cout << "The product of the two matrices is:" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << pro[i][j] << "\t";
+        }
+        cout << endl;
+    }
 }
