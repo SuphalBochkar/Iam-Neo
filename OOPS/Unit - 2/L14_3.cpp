ce578edd27fa71f3e634b9a2446bdafed86ae20c
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-16 22:57:27 +0530

    Create L14_3.cpp

---
+++ OOPS/Unit - 2/L14_3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string sen1, sen2;
+
+    getline(cin, sen1);
+    getline(cin, sen2);
+
+    cout << "The first string before swapping is : " << sen1 << endl;
+    cout << "The second string before swapping is : " << sen2 << endl;
+    cout << "The first string after swapping is: " << sen2 << endl;
+    cout << "The second string after swapping is: " << sen1 << endl;
+}
