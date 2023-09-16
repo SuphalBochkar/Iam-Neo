f4af1fcf47ccf4ef09e3991aa19c2b943dad20ae
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-16 22:58:24 +0530

    Vowels, Special, Reverse

--- OOPS/Unit - 2/L14_4.cpp
+++ OOPS/Unit - 2/L14_4.cpp
@@ -3,6 +3,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string reverseSentence(const string &sentence)
+{
+    string reversedSentence = "";
+    string word = "";
+
+    for (char ch : sentence)
+    {
+        if (ch == ' ')
+        {
+            reversedSentence = word + " " + reversedSentence;
+            word = "";
+        }
+        else
+        {
+            word += ch;
+        }
+    }
+
+    reversedSentence = word + " " + reversedSentence;
+
+    return reversedSentence;
+}
+
+void extractVowelsAndConsonants(const string &sentence, string &vowels, string &consonants)
+{
+    for (char ch : sentence)
+    {
+        if (isalpha(ch))
+        {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                vowels += ch;
+            }
+            else
+            {
+                consonants += ch;
+            }
+        }
+    }
+}
+
 int main()
 {
     string sentence;
