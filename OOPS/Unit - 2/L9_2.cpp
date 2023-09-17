73604f906647098e77756bb591b1316dc73e886b
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-17 22:59:04 +0530

    Create L9_2.cpp

---
+++ OOPS/Unit - 2/L9_2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    float a;
+    cin >> a;
+
+    char av;
+    cin >> av;
+
+    if (a > 0 && a < 2500.50)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
