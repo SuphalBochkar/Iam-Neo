89458904f8ee226e2db0766a59746541ccdad185
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-20 23:04:00 +0530

    Shift LL

---
+++ DSA/Unit - 2/L11_3.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void addNode(node *&list, int val);
+void shift(node *&list, int sh);
+void display(node *list);
+int length(node *head);
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    node *list = nullptr;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        addNode(list, a);
+    }
+
+    cout << "Original List: ";
+    display(list);
+
+    int b;
+    cin >> b;
+    shift(list, b);
+
+    cout << "\nRotated List: ";
+    display(list);
+}
+
