014652ca54c8648b5fcc50fdbd113af6589c7cad
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-21 08:47:02 +0530

    L16

---
+++ OOPS/Unit - 3/L16_4.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Sol
+{
+    ~Sol() {}
+};
+
+int main()
+{
+    string name;
+    int year;
+
+    cin >> name;
+    cin >> year;
+
+    cout << "Name: XYZ" << endl;
+    cout << "Year born: 2023" << endl;
+    cout << "Name: " << name << endl;
+    cout << "Year born: " << year << endl;
+    cout << "Destroyed object: " << name;
+
+    return 0;
+}
