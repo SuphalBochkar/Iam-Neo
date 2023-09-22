c8eb1e6721cf0efd8e866734a4bf673b8c46b941
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-22 23:05:51 +0530

    Update L12_1.cpp

--- DSA/Unit - 2/L12_1.cpp
+++ DSA/Unit - 2/L12_1.cpp
@@ -40,6 +40,40 @@ void printList(Node *head)
     cout << endl;
 }
 
+void intersection(Node *head1, Node *head2)
+{
+    unordered_set<int> hashSet;
+
+    Node *temp1 = head1;
+    while (temp1->next != head1)
+    {
+        hashSet.insert(temp1->data);
+        temp1 = temp1->next;
+    }
+    hashSet.insert(temp1->data);
+
+    Node *temp2 = head2;
+    
+    cout << "List 1: ";
+    printList(head1);
+    
+    cout << "List 2: ";
+    printList(head2);
+
+    cout << "Intersection: ";
+
+    while (temp2->next != head2)
+    {
+        if (hashSet.find(temp2->data) != hashSet.end())
+            cout << temp2->data << " ";
+        temp2 = temp2->next;
+    }
+    
+    if (hashSet.find(temp2->data) != hashSet.end())
+        cout << temp2->data << " ";
+    cout << endl;
+}
+
 int main()
 {
     int testCases;
