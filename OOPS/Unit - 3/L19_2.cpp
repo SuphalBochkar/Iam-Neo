a54a6f88ac292c1d0c26f60b602342a9472d77c7
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-25 08:36:56 +0530

    Create L19_2.cpp

---
+++ OOPS/Unit - 3/L19_2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float a, b, c, sum, pro;
+
+    cin >> a >> b >> c;
+
+    sum = a + b;
+    pro = b * c;
+
+    cout << fixed << setprecision(2);
+    cout << sum << endl;
+    cout << pro << endl;
+
+    return 0;
+}
