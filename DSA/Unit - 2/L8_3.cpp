07c2a64880ded0bf6a73aa9fd878a5eac887d87e
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-27 23:08:13 +0530

    Update L8_3.cpp

--- DSA/Unit - 2/L8_3.cpp
+++ DSA/Unit - 2/L8_3.cpp
@@ -11,3 +11,33 @@ void addNode(node *&list, string key);
 void insertNode(node *&list, string key, int pos);
 void print(node *list);
 
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    node *list = nullptr;
+
+    for (int i = 0; i < n; i++)
+    {
+        string a;
+        getline(cin, a);
+        addNode(list, a);
+    }
+    cout << "Current Linked List:" << endl;
+
+    print(list);
+    cout << endl;
+
+    string key;
+    int pos;
+    cin >> key >> pos;
+
+    insertNode(list, key, pos);
+
+    cout << "Updated Linked List:" << endl;
+    print(list);
+    return 0;
+}
+
