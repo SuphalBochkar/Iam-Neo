c8e93a255afe47ae6996d7ae5dc9ad64bc071fc4
Suphal Bochkar <suphalbochkar@gmail.com>
2023-09-27 23:08:22 +0530

    Update L8_3.cpp

--- DSA/Unit - 2/L8_3.cpp
+++ DSA/Unit - 2/L8_3.cpp
@@ -41,3 +41,60 @@ int main()
     return 0;
 }
 
+void addNode(node *&list, string key)
+{
+    node *n = new node;
+    n->data = key;
+    n->next = nullptr;
+
+    if (list == nullptr)
+    {
+        list = n;
+    }
+    else
+    {
+        node *ptr = list;
+        while (ptr->next != nullptr)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = n;
+    }
+}
+
+void insertNode(node *&list, string key, int pos)
+{
+    node *n = new node;
+    n->data = key;
+    n->next = nullptr;
+
+    if (pos == 1)
+    {
+        n->next = list;
+        list = n;
+        return;
+    }
+
+    node *ptr = list;
+    int c = 1;
+    while (c != pos - 1 && ptr != nullptr)
+    {
+        c++;
+        ptr = ptr->next;
+    }
+
+    if (ptr != nullptr)
+    {
+        n->next = ptr->next;
+        ptr->next = n;
+    }
+}
+
+void print(node *list)
+{
+    for (node *ptr = list; ptr != nullptr; ptr = ptr->next)
+    {
+        cout << ptr->data << " ";
+    }
+}
+
